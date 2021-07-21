e806f143a248ebeb532d19aa78dd124c7f28daa7
haksong <gkrthdz@naver.com>
2021-07-21 18:40:45 +0900

    # configured eureka client

---
+++ Consumer/src/main/java/com/hystrix/comsumer/ConsumerApplication.java
@@ -0,0 +1,52 @@
+package com.hystrix.comsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	@RestController
+	static class consumerController{
+		@GetMapping("/consumer")
+		@HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+				@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500"),
+				@HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000"),
+				@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10"),
+				@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+				@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")
+		}, threadPoolProperties = @HystrixProperty(name = "coreSize", value = "100"))
+		public String consumer(@RequestParam String path) {
+			System.out.println("log :: " + path);
+			
+			ResponseEntity<String> entity = new RestTemplate().getForEntity("http://127.0.0.1:8090/" + path, String.class);
+			
+			if(entity.getStatusCode() == HttpStatus.OK) {
+				return entity.getBody();
+			}
+			
+			throw new RuntimeException("supplier is not ok");
+		}
+		
+		private String fallback(String path) {
+			return "Hello Fallback";
+		}
+	}
+}
---
+++ Consumer/src/test/java/com/hystrix/comsumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hystrix.comsumer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
